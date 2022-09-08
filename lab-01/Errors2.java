fe6b6da2efaff13d03a7f66b611e06952453122e
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-08 13:29:35 -0400

    Added slash to make comment not a syntax error

--- lab-01/Errors2.java
+++ lab-01/Errors2.java
@@ -6,7 +6,7 @@ public class errors
 {
 public static void main (String[] args)
 {
-String Name; / Name of the user
+String Name; // Name of the user
 int number;
 int numSq;
 Scanner scan = new Scanner(System.in);
